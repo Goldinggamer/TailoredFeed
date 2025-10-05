d0376909eee24682f1d1ed6ef8e70b55cc206065
Goldinggamer <gagagarekt@gmail.com>
2025-10-06 01:15:37 +0200

    Titel übersetzung leicht verbessert

--- title_translation.py
+++ title_translation.py
@@ -8,7 +8,7 @@ def translate_with_ollama(text, target_language='German', model='phi4'):
     if target_language == "Deutsch":
         return text
 
-    prompt = f'Translate the following sentence to {target_language}. Respond only with the translation and NOTHING else:\n{text}'
+    prompt = f'Translate the following sentence to {target_language}. Respond ONLY with the translation and NOTHING else:\n{text}'
     response = ollama_client.chat(model=model, messages=[
         {'role': 'user', 'content': prompt}
     ])
