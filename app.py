c3773036baf65e810350ee8fc4d31c43e2cf6c33
Goldinggamer <gagagarekt@gmail.com>
2025-10-06 14:19:48 +0200

    Background scheduler auf 3 minuten gesetzt, damit tagesschau api nur 60 anfragen pro stunde zulässt

--- app.py
+++ app.py
@@ -276,7 +276,7 @@ if __name__ == '__main__':
 
 
     scheduler = BackgroundScheduler()
-    scheduler.add_job(background_task, 'interval', seconds=60)
+    scheduler.add_job(background_task, 'interval', minutes=3)
     scheduler.start()
     
     app.run(debug=True)
